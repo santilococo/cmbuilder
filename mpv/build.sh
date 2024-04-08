5dfd7616d52dbe5e6e1b3c67a639ba9e9d16e41a
Santiago Lo Coco <santilococo.01@gmail.com>
2024-04-09 00:06:11 +0200

    Get package version

--- mpv/build.sh
+++ mpv/build.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
-uname -a
-
-
 set -ex
+
+uname -a | grep -q "arm64"
 
 cd "$(dirname "$0")"
 
 baseFolder="$(pwd)"
 
-arch -arm64 brew install --build-from-source --only-dependencies mpv
-arch -arm64 brew install libplacebo dylibbundler
+brew install --build-from-source --only-dependencies mpv
+brew install libplacebo dylibbundler
 
 git clone "https://github.com/mpv-player/mpv"
 
@@ -23,12 +22,13 @@ meson compile -C build
 
 dylibbundler --bundle-deps --dest-dir build/mpv.app/Contents/MacOS/lib/ --install-path @executable_path/lib/ --fix-file build/mpv.app/Contents/MacOS/mpv
 
-find build/mpv.app
-
 hdiutil create -volname mpv -srcfolder build/mpv.app -ov -format UDZO mpv.dmg
 
 shasum -a 256 mpv.dmg > mpv.dmg.sha256sum
 
+./build/mpv --version | grep -oP 'mpv\s+v\K[\d.]+' > mpv.dmg.version
+
 mv mpv.dmg "$baseFolder"
 mv mpv.dmg.sha256sum "$baseFolder"
+mv mpv.dmg.version "$baseFolder"
 
