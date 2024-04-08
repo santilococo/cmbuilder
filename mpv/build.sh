c859635e0ff8bf98fe69ee1340e1fc07a3df1ea5
Santiago Lo Coco <santilococo.01@gmail.com>
2024-04-08 22:25:23 +0200

    Fix mpv.app

--- mpv/build.sh
+++ mpv/build.sh
@@ -18,13 +18,17 @@ meson compile -C build
 
 ./build/mpv --version
 
-./TOOLS/osxbundle.py build/mpv
+./TOOLS/osxbundle.py --skip-deps build/mpv
+
+dylibbundler --bundle-deps --dest-dir build/mpv.app/Contents/MacOS/lib/ --install-path @executable_path/lib/ --fix-file build/mpv.app/Contents/MacOS/mpv
+
+./build/mpv.app/Contents/MacOS/mpv --version
 
 find build/mpv.app
 
 hdiutil create -volname mpv -srcfolder build/mpv.app -ov -format UDZO mpv.dmg
 
-sh256sum mpv.dmg > mpv.dmg.sha256sum
+sha256sum mpv.dmg > mpv.dmg.sha256sum
 
 mv mpv.dmg "$baseFolder"
 mv mpv.dmg.sha256sum "$baseFolder"
